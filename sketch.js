ce53ed9c8a55b0058c90875e01a671c3258323f1
Anamol69 <146878034+Anamol69@users.noreply.github.com>
2023-11-22 14:08:41 -0600

    Added class "GoldenVariants"

--- sketch.js
+++ sketch.js
@@ -5,6 +5,38 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+class GoldenVariants {
+  constructor(test1, test2, test3, test4, test5, test6) {
+    this.test1 = test1;
+    this.test2 = test2;
+    this.test3 = test3;
+    this.test4 = test4;
+    this.test5 = test5;
+    this.test6 = test6;
+    this.x;
+    this.y;
+    this.dx;
+    this.dy;
+    this.width;
+  }
+  goldenCookie() {
+
+  }
+
+  goldenRush() {
+
+  }
+
+  goldenStar() {
+    
+  }
+
+  
+}
+
+
+
+
 let cookie;
 let goldenCookie;
 let menuMusic;
@@ -35,3 +67,6 @@ function draw() {
   fill(random(0, 255), random(0, 255), random(0, 255));
   circle(mouseX, mouseY, 100);
 }
+
+
+
